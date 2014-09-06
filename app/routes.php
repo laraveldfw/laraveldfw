fbcd1d3e66bd5e6716cc82b2c6d9d97eb15a3b9d
Tanner Hearne <tanner.hearne@gmail.com>
2014-09-06 15:05:32 -0500

    Updated for October 2014 Meetup information

--- app/routes.php
+++ app/routes.php
@@ -15,14 +15,13 @@ Route::get('/', function()
 {
   $data = [
     'hidemap' => true,
-    'meetupevent' => 'http://www.meetup.com/laravel-dallas-fort-worth/events/200296712/',
     'locationname' => 'Google Hangouts',
-    'locationurl' => 'https://plus.google.com/events/ct3s9qin5mcd7tpfnj6soheigbo',
-    'datetime' => 'Thursday, September 4th at 7:00pm',
-    'speaker' => 'Andrew Del Prete',
-    'speakerurl' => 'https://twitter.com/pathsofdesign',
-    'speakerimg' => 'img/andrew-del-prete.jpg',
-    'talk' => 'Laravel + Restangular'
+    'locationurl' => 'https://plus.google.com/events/cudpuacki09mdnse9uuv9pm9cs4',
+    'datetime' => 'Thursday, October 2nd at 7:00pm',
+    'speaker' => 'Osvaldo Brignoni',
+    'speakerurl' => 'https://twitter.com/obrignoni',
+    'speakerimg' => 'img/osvaldo-brignoni.jpg',
+    'talk' => 'Lexicon - an expressive template engine with extensible vocabulary'
   ];
 
   return View::make('home', compact('data'));
@@ -30,31 +29,16 @@ Route::get('/', function()
 
 Route::get('/ask', ['as' => 'ask', function()
 {
-  return Redirect::away('https://tannerhearne.typeform.com/to/OwBODn');
+  return Redirect::away('https://tannerhearne.typeform.com/to/Wm4Ygq');
 }]);
 
-Route::get('/hangout', array('as' => 'hangout', function()
+Route::get('/live', array('as' => 'live', function()
 {
-  return Redirect::away('https://plus.google.com/events/ct3s9qin5mcd7tpfnj6soheigbo');
-}));
-
-Route::get('/live', array('as' => 'hangout', function()
-{
-  return Redirect::away('https://plus.google.com/events/ct3s9qin5mcd7tpfnj6soheigbo');
+  return Redirect::away('https://plus.google.com/events/cudpuacki09mdnse9uuv9pm9cs4');
 }));
 
 
 Route::get('/rsvp', array('as' => 'rsvp', function()
 {
-  return Redirect::away('http://www.meetup.com/laravel-dallas-fort-worth/events/181020812/');
-}));
-
-Route::get('/dallas-maker-space', array('as' => 'rsvp', function()
-{
-  return Redirect::away('http://www.meetup.com/dallasmakerspace/events/182120322/');
-}));
-
-Route::get('/php-world', array('as' => 'rsvp', function()
-{
-  return Redirect::away('http://world.phparch.com');
+  return Redirect::away('http://www.meetup.com/laravel-dallas-fort-worth/events/195798912/');
 }));
