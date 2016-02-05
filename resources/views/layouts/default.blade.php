5af5f181ae35749bfeb7c1fa1b517fdec777def6
Tanner Hearne <tanner.hearne@gmail.com>
2016-02-04 19:43:41 -0600

    Add Google Meta Tag

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title', 'Laravel Dallas/Fort Worth')</title>
     <meta name="description" content="@yield('description', 'Laravel DFW is a community of application developers and designers that want to learn, communicate, and collaborate.')">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="google-site-verification" content="Oya4QNGKhL5X4zUOMDE1eFxAzosmSZS8dSq8L76kLVo" />
 
     {{-- Fonts and CSS --}}
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700' rel='stylesheet' type='text/css'>
