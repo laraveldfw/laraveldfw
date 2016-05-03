d531d859ad1d71d1b7fafc367bce023c05da396a
freshyseth <freshyseth@gmail.com>
2016-05-03 18:29:36 -0500

    Update date and talk information.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,16 +14,16 @@
 Route::get('/', function () {
   $data = [
     'online' => false,
-    'datetime' => 'Thursday, April 7th at 7:00pm',
     'speaker' => 'Nathan Barrett',
     'speakerurl' => 'https://www.linkedin.com/in/nathan-barrett-06b91512',
     'speakerimg' => 'images/nathan-barrett.jpg',
-    'talk' => 'Custom forms with Laravel and Angular 1.x',
     'additionalinfo' => 'Taylor Otwell will also be joining this month in-person! And we&rsquo;ll be right next door to the Lone Star PHP conference!'
     'locationname' => 'Lupe&rsquo;s TexMex Grill',
     'locationaddress' => '2200 Airport Freeway, Suite #505, Bedford, TX 76022',
     'locationphone' => '(817) 545-5004',
     'locationurl' => 'http://www.lupestxmx.com/',
+    'datetime' => 'Thursday, May 5th at 7:00pm',
+    'talk' => 'In-person MeetUp!',
   ];
 
   return view('home', compact('data'));
